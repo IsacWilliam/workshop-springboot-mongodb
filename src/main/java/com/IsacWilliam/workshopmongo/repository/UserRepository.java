b8b538cd4761f02890a1ef809f86e68f1d520d14
Isac William <hailisac@hotmail.com>
2023-07-23 16:47:55 -0300

    Conectando ao MongoDB com Repository e Service

---
+++ src/main/java/com/IsacWilliam/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.IsacWilliam.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.IsacWilliam.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
