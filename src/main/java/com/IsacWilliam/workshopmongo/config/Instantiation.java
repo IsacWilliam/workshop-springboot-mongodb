7354251e80ba6d397706f10981626a385f4a27e8
Isac William <hailisac@hotmail.com>
2023-07-24 14:49:09 -0300

    Operação de Inserção da Base de Dados

---
+++ src/main/java/com/IsacWilliam/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.IsacWilliam.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.IsacWilliam.workshopmongo.domain.User;
+import com.IsacWilliam.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@email.com");
+		User alex = new User(null, "Alex Green", "alex@email.com");
+		User bob = new User(null, "Bob Grey", "bob@email.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
